bee8e476e171e3b17479dcb1db30ab83629a94af
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-14 19:29:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,9 @@
-# name method 
+# Output phrase with name 
 def introduction(name)
   puts "Hi, my name is ${name}"
 end
 
-#
+# Output phrase with name and language
 def introduction_with_language(name,language)
   puts "Hi, my name is ${name} and I am learning to program in ${language}"
 end
