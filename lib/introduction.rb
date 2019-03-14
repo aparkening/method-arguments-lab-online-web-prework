469f7f3564bfa0da8eda352e7845c12e6e405253
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-14 19:34:13 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,12 @@
+# Output phrase with name 
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+# Output phrase with name and language
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+#introduction("Josh")
+#introduction_with_language("Dan","Ember")
